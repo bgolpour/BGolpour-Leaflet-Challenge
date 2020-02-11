6dc2690c8e3a20c04e82b6eb4dc9e62e12273805
Bax <b.golpour@gmail.com>
2020-02-11 16:39:36 -0500

    new commit

--- Startercode/static/js/logic.js
+++ Startercode/static/js/logic.js
@@ -2,8 +2,8 @@
   
 var API_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 console.log (API_quakes)
-
 var API_plates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
 console.log (API_plates)
 
 function markerSize(magnitude) {
